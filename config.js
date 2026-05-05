688ebd36e9fa3444e659c11dca645ae6df91a31a
DeveloperTeamDbug <publicservers360@gmail.com>
2026-05-05 14:42:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'put your session id here',
+SESSION_ID:'z151hDpL#-YBEgxNwqtWtdSVAyY8OvX-bwd_lZVOgHLPZEYvrodk',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'false' : process.env. MV_BLOCK,    
